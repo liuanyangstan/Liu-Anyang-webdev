3fab79d453d3c78e56b404c68c635b0073d7d8e3
stan <stan@standeMacBook-Pro.local>
2017-05-19 21:05:30 -0700

    Complete

--- server.js
+++ server.js
@@ -8,11 +8,12 @@ var express = require('express');
 //initialize app as an express application
 var app = express();
 
-//var ipaddress = '127.0.0.1';                    //local host: fixed
+var ipaddress = '127.0.0.1';                    //local host: fixed
 var port      = process.env.PORI || 3000;
 
-app.use(express.static(__dirname+'/public'));
-//app.listen(port, ipaddress);
-app.listen(port);
+app.use(express.static(__dirname+'/public'));   //folder name : public
+app.listen(port, ipaddress);
+
+//app.listen(port);
 
 console.log("hello world!");
