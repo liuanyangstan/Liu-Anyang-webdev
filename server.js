ed50b287e023cc3b261731a282f038af8cb92e34
stan <stan@standeMacBook-Pro.local>
2017-05-19 18:47:57 -0700

    new platform

--- server.js
+++ server.js
@@ -3,14 +3,16 @@
  */
 //using express with node js
 var express = require('express');
+//var bodyParser = require('body-parser');
 
 //initialize app as an express application
 var app = express();
 
-var ipaddress = '127.0.0.1';
-var port      = 3000;
+//var ipaddress = '127.0.0.1';                    //local host: fixed
+var port      = process.env.PORI || 3000;
 
 app.use(express.static(__dirname+'/public'));
-app.listen(port, ipaddress);
+//app.listen(port, ipaddress);
+app.listen(port);
 
-console.log("hello world, well done!");
+console.log("hello world!");
