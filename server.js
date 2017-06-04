b706125a165305eb6a62da0f2373ddebe6c043de
liuanyangstan <liuanyangstan@gmail.com>
2017-06-04 16:00:33 -0700

    HW2

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var express = require('express');
 var app = express();
 
 var ipaddress = '127.0.0.1';                    //local host: fixed
-var port      = process.env.PORI || 3000;
+var port      = process.env.PORI || 5000;
 
 app.use(express.static(__dirname+'/public/assignment'));   //folder name : public
 app.listen(port, ipaddress);
