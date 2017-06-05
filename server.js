45b17ef318407b9612c1d776cf6df74ab87c9e96
liuanyangstan <liuanyangstan@gmail.com>
2017-06-04 17:17:55 -0700

    HW2-2.1

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
  */
 //using express with node js
 
-/**
+
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -13,14 +13,15 @@ var app = express();
 var ipaddress = '127.0.0.1';                    //local host: fixed
 var port      = process.env.PORI || 5000;
 
-app.use(express.static(__dirname+'/public'));   //folder name : public
+app.use(express.static(__dirname+'/public/assignment'));   //folder name : public
 app.listen(port, ipaddress);
 
-//app.listen(port);
+app.listen(port);
 
 console.log("hello world!");
-**/
 
+
+/**
 var express = require('express');
 var app = express();
 
@@ -38,3 +39,4 @@ app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
 });
+ **/
