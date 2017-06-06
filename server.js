538ea13ea9e80287ee529de8ddd134f0a3ad7537
liuanyangstan <liuanyangstan@gmail.com>
2017-06-05 17:12:07 -0700

    HW2-2.1

--- server.js
+++ server.js
@@ -10,32 +10,34 @@ var express = require('express');
 //initialize app as an express application
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname+'/'));
+app.get('/test', function (req, res) {
+    res.send('Text information received')
+});
 
-app.listen(app.get('port'), function() {
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public/assignment'));
+
+app.listen(app.get('port'), function () {
     console.log('Node app is running on port', app.get('port'));
 });
 
 
-
-
 /**
-var express = require('express');
-var app = express();
+ var express = require('express');
+ var app = express();
 
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+ var bodyParser = require('body-parser');
+ app.use(bodyParser.json());
+ app.use(bodyParser.urlencoded({ extended: true }));
 
-// configure a public directory to host static content
-app.use(express.static(__dirname + '/public/assignment'));
+ // configure a public directory to host static content
+ app.use(express.static(__dirname + '/public/assignment'));
 
 
 
-app.set('port', (process.env.PORT || 5000));
+ app.set('port', (process.env.PORT || 5000));
 
-app.listen(app.get('port'), function() {
+ app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
 });
  **/
