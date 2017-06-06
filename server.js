6c8b7a7fe6f5325c915c2fbbadd2289f971b44f0
liuanyangstan <liuanyangstan@gmail.com>
2017-06-05 17:24:49 -0700

    HW2-2.1

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/test', function (req, res) {
 });
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(process.env.PWD + '/public/assignment'));
+app.use(express.static(__dirname + '/public/assignment'));
 
 app.listen(app.get('port'), function () {
     console.log('Node app is running on port', app.get('port'));
