ff076d02a36c42e598e9cbc07dace74c85d9add9
liuanyangstan <liuanyangstan@gmail.com>
2017-06-22 15:57:32 -0700

    Assignment3

--- public/ngExample/config.js
+++ public/ngExample/config.js
@@ -1,7 +1,34 @@
 /**
  * Created by stan on 6/2/17.
  */
-(function (){
+(function () {
     angular
-        .module()
+        .module("WhiteBoardApp")
+        .config(function($routeProvider)
+        {
+            $routeProvider
+                .when("/",
+                    {
+                        /*templateUrl: "courseList/courseList.view.html",*/
+                        templateUrl: "courseList.view.html",
+                        controller: "courseList.controller"
+                    })
+                .when("/course/:id",
+                    {
+                        templateUrl: "courseEdit.view.html",
+                        controller: "courseEdit.controller"
+                    })
+                .when("/profile",
+                    {
+                        /*templateUrl: "profile/profile.view.html",*/
+                        templateUrl:"profile.view.html",
+                        controller: "profile.controller"
+                    })
+                .when("/message",
+                    {
+                        /*templateUrl: "profile/profile.view.html",*/
+                        templateUrl:"message.view.html",
+                        controller: "profile.controller"
+                    })
+        });
 })();
