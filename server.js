dfec8442ec2f6c2b8114370b4674de7f5fbf3849
liuanyangstan <liuanyangstan@gmail.com>
2017-08-13 21:36:12 -0700

    Project1.0

--- server.js
+++ server.js
@@ -9,13 +9,14 @@ var express = require('express');
 var app = express();
 
 var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+
+// load and configure the server to use cookie based session support.
 var passport = require('passport');
 var cookieParser = require('cookie-parser');
 var session      = require('express-session');
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(session({
     secret: process.env.SESSION_SECRET || "This is a secret",
@@ -27,11 +28,13 @@ app.use(cookieParser());
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.set('port', (process.env.PORT || 5000));
- app.use(express.static(__dirname + '/public/assignment'));
+app.set('port', (process.env.PORT || 3000));
+app.use(express.static(__dirname + '/public'));
 
 app.listen(app.get('port'), function () {
     console.log('Node app is running on port', app.get('port'));
 });
 
-require("./assignment/app.js")(app);
+// configure a public directory to host static content
+// require("./assignment/app.js")(app);
+require("./project/app.js")(app);
